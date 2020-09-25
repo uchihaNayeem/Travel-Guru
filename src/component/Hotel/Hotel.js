5dd7bfe54a0fd2835ac55a00ba33bf7e919a108d
uchihaNayeem <uchiha.nayeem@gmail.com>
2020-09-25 12:50:29 +0600

    google Map

--- src/component/Hotel/Hotel.js
+++ src/component/Hotel/Hotel.js
@@ -51,6 +51,27 @@ const Hotel = () => {
           <br/>
         </div>
       </div>
+
+
+
+      <div className="mt-12">
+                
+
+
+<iframe src="https://www.google.com/maps/dir/Chattogram/Dhaka/@22.0682122,89.608882,6.63z/data=!4m14!4m13!1m5!1m1!1s0x30acd8a64095dfd3:0x5015cc5bcb6905d9!2m2!1d91.7831819!2d22.356851!1m5!1m1!1s0x3755b8b087026b81:0x8fa563bbdd5904c2!2m2!1d90.4125181!2d23.810332!3e0" 
+                width="450" 
+                height="700" 
+                frameBorder="0" 
+                style={{ border: 0, marginTop: '20px', padding: '10px', borderRadius: '20px' }} 
+                allowFullScreen="" 
+                ariaHidden="false" 
+                tabIndex="0">
+                </iframe>
+
+
+            </div>
+
+
       
     </div>
   );
